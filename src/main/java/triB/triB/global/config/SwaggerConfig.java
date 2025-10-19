e41ba2cc94cb45db77cf3ece262d7eeb66f5f368
ej9374 <109258160+ej9374@users.noreply.github.com>
2025-10-20 02:33:01 +0900

    feat:swaggerConfig Authorization 추가

--- src/main/java/triB/triB/global/config/SwaggerConfig.java
+++ src/main/java/triB/triB/global/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package triB.triB.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,8 +14,16 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
+                .info(apiInfo())
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("bearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("JWT 토큰을 입력하세요. (Bearer prefix는 자동으로 추가됩니다.)")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 
     private Info apiInfo(){
