04d153449ede57b64479882eeb0d5df1cbcde209
ej9374 <109258160+ej9374@users.noreply.github.com>
2025-11-06 18:17:27 +0900

    fix: error code 자세히수정

--- src/main/java/triB/triB/global/exception/ErrorCode.java
+++ src/main/java/triB/triB/global/exception/ErrorCode.java
@@ -11,8 +11,9 @@ public enum ErrorCode {
     CONFLICT_FRIENDSHIP_TO_ME(HttpStatus.CONFLICT, "CONFLICT_FRIENDSHIP_TO_ME","상대가 이미 보낸 친구 요청이 대기 중입니다. 수신함에서 확인하세요."),
     CONFLICT_FRIENDSHIP_TO_OTHER(HttpStatus.CONFLICT, "CONFLICT_FRIENDSHIP_TO_OTHER", "이미 친구요청을 보낸 유저입니다."),
 
-    NO_FCM_TOKEN(HttpStatus.UNAUTHORIZED, "NO_FCM_TOKEN", "FCM 토큰이 저장되지 않았습니다.");
-    ;
+    NO_FCM_TOKEN(HttpStatus.UNAUTHORIZED, "NO_FCM_TOKEN", "FCM 토큰이 저장되지 않았습니다."),
+    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "INVALID_REFRESH_TOKEN", "refresh token이 유효하지 않습니다.");
+
 
     private final HttpStatus httpStatus;
     private final String code;
