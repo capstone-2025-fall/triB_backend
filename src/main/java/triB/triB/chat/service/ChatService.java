99f8c80b5e3c97017b1de97a8326469e7c6e4aaf
ej9374 <109258160+ej9374@users.noreply.github.com>
2025-11-18 16:03:25 +0900

    chore: 모델 통신 전달 request places 확인

--- src/main/java/triB/triB/chat/service/ChatService.java
+++ src/main/java/triB/triB/chat/service/ChatService.java
@@ -180,6 +180,10 @@ public class ChatService {
                 }
             }
 
+            ObjectMapper mapper = new ObjectMapper();
+            log.info("places: {}",
+                    mapper.writeValueAsString(places));
+
             ModelRequest modelRequest = ModelRequest.builder()
                     .days((int) ChronoUnit.DAYS.between(room.getStartDate(), room.getEndDate()) + 1)
                     .startDate(room.getStartDate().toString())
